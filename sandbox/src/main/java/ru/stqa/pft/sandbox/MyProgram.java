7ef469712ff394cd1676122145a46cc2de4f5b95
Евгений Семенов <semenov.evgenij@gmail.com>
2018-12-17 11:29:52 +0400

    12

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyProgram.java
@@ -3,8 +3,12 @@ package ru.stqa.pft.sandbox;
 public class MyProgram {
 
     public static void main(String[] args) {
+        int x = 11111;
+        int y = 1111111;
+        int z;
+        z = x * y;
 
-        System.out.println("Hello, World!");
+
+        System.out.println(z);
     }
-
 }
